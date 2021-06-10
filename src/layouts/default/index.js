e862d7779a792bc11b597807255c69a65968d6a1
topdev10 <k100cool@outlook.com>
2021-06-10 22:12:26 +0800

    styling layout

--- src/layouts/default/index.js
+++ src/layouts/default/index.js
@@ -4,10 +4,20 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
 
 import Header from './header';
+import Footer from './footer';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    height: '100vh'
+  },
+  container: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  main: {
+    flex: 1
   }
 }));
 
@@ -16,11 +26,12 @@ const DefaultLayout = ({children}) => {
 
   return (
     <div className={classes.root}>
-      <Container>
+      <Container className={classes.container}>
         <Header />
-        <div>
+        <div className={classes.main}>
           {children}
         </div>
+        <Footer />
       </Container>
     </div>
   );
